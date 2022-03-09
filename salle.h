f4904621f786f60c0c034d68cfd2f47ae7e085c2
abryckaert <bryckaert.anthony@gmail.com>
2022-03-09 08:52:12 +0100

    Création de la classe Salle

---
+++ salle.h
@@ -0,0 +1,14 @@
+#ifndef SALLE_H
+#define SALLE_H
+
+#include <QMainWindow>
+
+class Salle : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Salle(QWidget *parent = nullptr);
+    ~Salle();
+};
+#endif // SALLE_H
